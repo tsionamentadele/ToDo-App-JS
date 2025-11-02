e04b631e451db120f43d8a60fcac81b511e21e0c
zion <tsionamentadele@gmail.com>
2025-11-02 14:20:51 +0300

    add a completed and delete task feature

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const task=document.querySelector("#task")
 const add=document.querySelector("#add")
 const taskList=document.querySelector("#task-list")
+const li=document.querySelector("li")
 
 add.addEventListener("click",()=>{
     if(task.value.trim()==""){
@@ -8,12 +9,35 @@ add.addEventListener("click",()=>{
     }
     let task_value=task.value.trim()
     let li=document.createElement("li")
-    li.textContent=task_value
-    taskList.append(li)
+    let delete_task=document.createElement("button")
+    delete_task.textContent="Delete"
+    delete_task.classList.add("delete_task")
+
+    const span = document.createElement("span");
+    span.textContent = task_value;
+
+    li.append(span);
+    li.append(delete_task);
+    taskList.append(li);
+
+
     task.value = "";
     task.focus();
+
+    delete_task.addEventListener("click",()=>{
+        li.remove()
+    })
 
 })
 task.addEventListener("keypress", (e) => {
   if (e.key === "Enter") add.click();
 });
+
+taskList.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.tagName === "SPAN") {
+    target.classList.toggle("done"); 
+  }
+});
+
+
